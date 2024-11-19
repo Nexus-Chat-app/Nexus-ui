e693866711480257feb32e566f4191ef6295375f
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-19 18:50:43 +0100

    Update Sidebar Buttons styles

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -1,37 +1,44 @@
-import { MessageSquare, Users, Radio, Settings } from 'lucide-react';
+import { MessageSquare, Users, Radio, Settings } from "lucide-react";
 
 const navItems = [
-  { icon: MessageSquare, label: 'Messages', active: true },
-  { icon: Users, label: 'Groups' },
-  { icon: Radio, label: 'Channels' },
-  { icon: Settings, label: 'Settings' }
+  { icon: MessageSquare, label: "Messages", active: true },
+  { icon: Users, label: "Groups" },
+  { icon: Radio, label: "Channels" },
+  { icon: Settings, label: "Settings" },
 ];
 
 export default function Sidebar() {
   return (
     <div className="w-16 rounded-l-xl h-full glass border-r border-white/10 flex flex-col items-center py-6">
-      <div className="w-12 h-12 bg-emerald-500/80 backdrop-blur-lg rounded-full mb-8 flex items-center justify-center">
-        <span className="text-white font-semibold text-xl">W</span>
+      <div className="w-10 backdrop-blur-lg rounded-full mb-8 flex items-center justify-center">
+          <img
+            src="./logoBlack.png"
+            className="w-10 rounded-full dark:hidden"
+            alt="Logo"
+          />
+          <img
+            src="./logoWhite.png"
+            className="w-10 rounded-full hidden dark:block"
+            alt="Logo"
+          />
       </div>
-      
+
       <nav className="flex-1 flex flex-col gap-4">
         {navItems.map((item) => (
           <button
             key={item.label}
-            className={`p-3 rounded-xl transition-all duration-200 group ${
-              item.active 
-                ? 'bg-emerald-500/80 backdrop-blur-lg' 
-                : 'glass-hover'
+            className={`p-2 rounded-full transition-all duration-200 text-black dark:text-white group ${
+              item.active ? "bg-blue-300/80 dark:bg-red-700/80 backdrop-blur-lg " : "glass-hover"
             }`}
             title={item.label}
           >
-            <item.icon className="w-6 h-6" />
+            <item.icon className="w-6 h-6 stroke-current" />
           </button>
         ))}
       </nav>
-      
+
       <div className="mt-auto">
-        <button className="w-12 h-12 rounded-full overflow-hidden ring-2 ring-white/20">
+        <button className="w-8 h-8 rounded-full overflow-hidden ring-2 ring-white/20">
           <img
             src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=100&h=100&fit=crop"
             alt="Profile"
