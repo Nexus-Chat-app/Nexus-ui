01a52981788319d0ff45d111c4cdb26110edc300
Zaiidmo <Zaiidmoumnii@gmail.com>
2024-11-20 09:55:46 +0100

    Updating Components View Styles

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -1,6 +1,5 @@
 import { RootState } from "@/store/store";
 import { MessageSquare, Users, LogIn, UserPlus } from "lucide-react";
-import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -4,7 +4,7 @@ import LoginForm from "@/components/auth/LoginForm";
 export default function Login() {
   return (
     <div className="h-full flex items-center justify-center glass backdrop-filter backdrop-blur-xl bg-white/60 dark:bg-gray-900/60 border border-gray-200 dark:border-gray-700 rounded-t-xl md:rounded-r-xl md:rounded-tl-none">
-      <div className="w-full max-w-md space-y-8 p-8 rounded-t-xl md:rounded-xl shadow-lg sm:max-w-[425px] md:max-w-screen-md md:mx-auto backdrop-filter backdrop-blur-xl bg-blue-500/60 dark:bg-red-900/40 border border-blue-200 dark:border-red-700 glass">
+      <div className="w-full max-w-md space-y-8 p-8 rounded-t-xl md:rounded-xl shadow-lg sm:max-w-[425px] md:max-w-screen-md md:mx-auto backdrop-filter backdrop-blur-xl !bg-blue-500/60 dark:!bg-red-900/40 border border-blue-200 dark:border-red-700 glass">
         <div className="text-center">
           <h2 className="mt-6 text-3xl md:text-5xl lg:text-7xl font-bold text-gray-900 dark:text-white font-titles">
             Welcome To Nexus
@@ -26,7 +26,7 @@ export default function Login() {
           <span className="text-sm text-gray-400">Don't have an account? </span>
           <Link
             to="/register"
-            className="text-sm font-medium text-black dark:text-white hover:text-primary-dark dark:text-primary-light dark:hover:text-primary transition-colors"
+            className="text-sm font-medium text-white hover:text-primary-dark dark:text-primary-light dark:hover:text-primary transition-colors"
           >
             Sign up
           </Link>
